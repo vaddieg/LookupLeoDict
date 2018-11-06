21958892246551943e304ab62a95fdf41dcd0ed7
Vadym Zimin <vaddieg@gmail.com>
2018-11-06 14:21:04 +0100

    Initial Commit

---
+++ LookupLeoDict/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LookupLeoDict
+//
+//  Created by vad on 11/6/18.
+//  Copyright © 2018 vaddieg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
