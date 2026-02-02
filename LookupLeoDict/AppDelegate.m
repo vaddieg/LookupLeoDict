adc36ba63f0c6dddbab12eb66c51471407172228
vaddieg <vzimin@anydesk.com>
2026-02-02 16:57:47 +0100

    Initial Commit

---
+++ LookupLeoDict/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  LookupLeoDict
+//
+//  Created by vad on 11/6/18.
+//  Copyright © 2018 vaddieg. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
